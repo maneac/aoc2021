4dc8cba847e3883f14925677e3f2ee55dde979a0
Ben Atkinson <benatkinson027@gmail.com>
2021-12-01 23:59:32 +0000

    [Day 1] Add TS solutions

---
+++ ts/README.md
@@ -0,0 +1,12 @@
+# TypeScript (Deno)
+
+Solutions for Advent of Code 2021, written in TypeScript, to be run using Deno.
+
+## Running
+
+Assuming Deno is installed:
+
+```bash
+# From this repository's root
+deno run --allow-read ./ts/run.ts <day_num>
+```
---
+++ ts/run.ts
@@ -0,0 +1,17 @@
+const readDay: () => number = () => {
+  if (Deno.args.length > 0) {
+    return +Deno.args[1];
+  }
+
+  // default to latest day
+  let day = 0;
+  for (const entry of Deno.readDirSync(".")) {
+    if (entry.isDirectory) {
+      day++;
+    }
+  }
+  return day;
+};
+
+const mod = await import("./day_" + readDay() + "/main.ts");
+mod.main();
