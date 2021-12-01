c1ff3dfea06f9e68aacb0c930deccded5defe5ea
Ben Atkinson <benatkinson027@gmail.com>
2021-12-01 23:58:58 +0000

    [Setup] Create language templates alongside READMEs

--- utils/fetch_day/src/main.rs
+++ utils/fetch_day/src/main.rs
@@ -44,15 +44,9 @@ fn main() {
         .collect::<Vec<String>>()
         .join("\n");
 
-    for lang in ["go", "ts", "rs"] {
-        let lang_instruction_file = Path::new(lang)
-            .join(format!("day_{}", &day))
-            .join("README.md");
-        if !lang_instruction_file.parent().unwrap().exists() {
-            std::fs::create_dir_all(&lang_instruction_file.parent().unwrap()).unwrap();
-        }
-        write(lang_instruction_file, &output).unwrap();
-    }
+    add_ts_template(&output, day);
+    add_go_template(&output, day);
+    add_rs_template(&output, day);
 
     let data_file = Path::new("data").join(format!("day_{}.txt", &day));
     if !data_file.exists() {
@@ -148,4 +142,189 @@ fn recursive_parse<'a>(day_url: &str, input: &mut Peekable<Chars<'a>>) -> String
     }
 
     output
+}
+
+fn add_ts_template(readme: &str, day: usize) {
+    let lang_instruction_dir = Path::new("ts").join(format!("day_{}", &day));
+
+    if !lang_instruction_dir.exists() {
+        std::fs::create_dir_all(&lang_instruction_dir).unwrap();
+    }
+    write(lang_instruction_dir.join("README.md"), &readme).unwrap();
+
+    write(
+        lang_instruction_dir.join("main.ts"),
+        r#"export function readData() {
+    throw new Error("unimplemented");
+}
+
+export function part1(_data: any) {
+    throw new Error("unimplemented");
+}
+
+export function part2(_data: any) {
+    throw new Error("unimplemented");
+}
+
+export function main() {
+    const data = readData();
+  
+    console.log("Part 1: ", part1(data));
+    console.log("Part 2: ", part2(data));
+}"#,
+    )
+    .unwrap();
+
+    write(
+        lang_instruction_dir.join("main.test.ts"),
+        r#"import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import { part1, part2, readData } from "./main.ts";
+
+Deno.test("part 1 real", () => {
+    const input = readData();
+
+    assertEquals(part1(input), 0);
+});
+
+Deno.test("part 2 real", () => {
+    const input = readData();
+
+    assertEquals(part2(input), 0);
+});"#,
+    )
+    .unwrap();
+}
+
+fn add_go_template(readme: &str, day: usize) {
+    let lang_instruction_dir = Path::new("go").join(format!("day_{}", &day));
+
+    if !lang_instruction_dir.exists() {
+        std::fs::create_dir_all(&lang_instruction_dir).unwrap();
+    }
+    write(lang_instruction_dir.join("README.md"), &readme).unwrap();
+
+    write(
+        lang_instruction_dir.join("main.go"),
+        r#"package main
+
+import "log"
+
+func main() {
+    data := readData()
+
+    log.Println("Part 1: ", part1(data))
+    log.Println("Part 2: ", part2(data))
+}
+        
+func readData() []string {
+    return nil
+}
+
+func part1(input []string) string {
+    panic("unimplemented")
+}
+
+func part2(input []string) string {
+    panic("unimplemented")
+}"#,
+    )
+    .unwrap();
+
+    write(
+        lang_instruction_dir.join("main_test.go"),
+        r#"package main
+
+import "testing"
+
+func TestPart1Real(t *testing.T) {
+    data := readData()
+
+    expected := ""
+    actual := part1(data)
+
+    if actual != expected {
+        t.Fatalf("Expected: %v\nActual: %v", expected, actual)
+    }
+}
+
+func TestPart2Real(t *testing.T) {
+    data := readData()
+
+    expected := ""
+    actual := part2(data)
+
+    if actual != expected {
+        t.Fatalf("Expected: %v\nActual: %v", expected, actual)
+    }
+}
+"#,
+    )
+    .unwrap();
+}
+
+fn add_rs_template(readme: &str, day: usize) {
+    let lang_instruction_dir = Path::new("rs").join(format!("day_{}", &day));
+
+    if !lang_instruction_dir.exists() {
+        std::fs::create_dir_all(&lang_instruction_dir.join("src")).unwrap();
+    }
+    write(lang_instruction_dir.join("README.md"), &readme).unwrap();
+
+    write(
+        lang_instruction_dir.join("Cargo.toml"),
+        format!(
+            r#"[package]
+name = "day_{}"
+version = "0.1.0"
+edition = "2021"
+
+[dependencies]
+"#,
+            day
+        ),
+    )
+    .unwrap();
+
+    write(
+        lang_instruction_dir.join("src").join("main.rs"),
+        r#"fn main() {
+    let data = read_data();
+
+    println!("Part 1: {}", part_1(&data));
+    println!("Part 2: {}", part_2(&data));
+}
+
+fn read_data() -> Vec<String> {
+    todo!()
+}
+
+fn part_1(_input: &Vec<String>) -> String {
+    todo!()
+}
+
+fn part_2(_input: &Vec<String>) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_real() {
+        let data = read_data();
+
+        assert_eq!("", part_1(&data));
+    }
+
+    #[test]
+    fn test_part_2_real() {
+        let data = read_data();
+
+        assert_eq!("", part_2(&data));
+    }
+}
+"#,
+    )
+    .unwrap();
 }
