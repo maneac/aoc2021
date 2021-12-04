7fdd20c316c92a77aaab3ce068282315a8f56bff
Ben Atkinson <benatkinson027@gmail.com>
2021-12-04 14:03:54 +0000

    [Optimise] Use iterator windows for day 1

--- rs/day_1/src/main.rs
+++ rs/day_1/src/main.rs
@@ -18,52 +18,19 @@ fn read_data(data_dir: &str) -> Vec<usize> {
         .collect()
 }
 
-fn part_1(input: &[usize]) -> String {
+fn part_1(input: &[usize]) -> usize {
     input
-        .iter()
-        .fold((0usize, None), |(mut acc, last_val_opt), val| {
-            let last_val = match last_val_opt {
-                Some(v) => v,
-                None => return (0, Some(val)),
-            };
-
-            if val > last_val {
-                acc += 1;
-            }
-
-            (acc, Some(val))
-        })
-        .0
-        .to_string()
+        .windows(2)
+        .filter(|window| window[1] > window[0])
+        .count()
 }
 
-fn part_2(input: &[usize]) -> String {
+fn part_2(input: &[usize]) -> usize {
     input
-        .iter()
-        .fold((0usize, [None; 3]), |(mut acc, last_vals_opts), val| {
-            let lv0 = match last_vals_opts[0] {
-                Some(v) => v,
-                None => return (0, [Some(val), None, None]),
-            };
-
-            let lv1 = match last_vals_opts[1] {
-                Some(v) => v,
-                None => return (0, [Some(lv0), Some(val), None]),
-            };
-
-            let lv2 = match last_vals_opts[2] {
-                Some(v) => v,
-                None => return (0, [Some(lv0), Some(lv1), Some(val)]),
-            };
-
-            if [*lv1, *lv2, *val].iter().sum::<usize>() > [*lv0, *lv1, *lv2].iter().sum() {
-                acc += 1;
-            }
-
-            (acc, [Some(lv1), Some(lv2), Some(val)])
-        })
-        .0
-        .to_string()
+        .windows(3)
+        .zip(input[1..].windows(3))
+        .filter(|(old, new)| new.iter().sum::<usize>() > old.iter().sum())
+        .count()
 }
 
 #[cfg(test)]
@@ -74,27 +41,27 @@ mod tests {
     fn test_part_1_real() {
         let data = read_data("../../data");
 
-        assert_eq!("1374", part_1(&data));
+        assert_eq!(1374, part_1(&data));
     }
 
     #[test]
     fn test_part_2_real() {
         let data = read_data("../../data");
 
-        assert_eq!("1418", part_2(&data));
+        assert_eq!(1418, part_2(&data));
     }
 
     #[test]
     fn test_part_1_example() {
         let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!("7", part_1(&data));
+        assert_eq!(7, part_1(&data));
     }
 
     #[test]
     fn test_part_2_example() {
         let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!("5", part_2(&data));
+        assert_eq!(5, part_2(&data));
     }
 }
